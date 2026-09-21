f780947a14ecaaecf7e3adb30695e73faeb80062
Anima2099 <Anima2099@proton.me>
2026-09-21 21:25:26 +0900

    Forward failed task support report requests

--- ui/widgets/download_task_list.py
+++ ui/widgets/download_task_list.py
@@ -27,6 +27,7 @@ class DownloadTaskList(QWidget):
     path_changed = Signal(str)
     thumbnail_recovery_requested = Signal(str)
     subtitle_recovery_requested = Signal(str)
+    problem_report_requested = Signal(str)
 
     def __init__(self, tasks: list[DownloadTask]) -> None:
         super().__init__()
@@ -86,6 +87,9 @@ class DownloadTaskList(QWidget):
         )
         card.subtitle_recovery_requested.connect(
             self.subtitle_recovery_requested
+        )
+        card.problem_report_requested.connect(
+            self.problem_report_requested
         )
         connect_ms = (perf_counter() - connect_started) * 1000.0
 
